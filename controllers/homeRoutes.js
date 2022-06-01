b92150ba7bddcb8bae39c2806ba5b1590d800e98
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-05-31 22:54:55 -0700

    finished models and created homepage and post page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const { Project, User } = require('../models');
+const { Post, User, Comment } = require("../models");
 // const withAuth = require('../utils/auth');
 
 router.get("/", async (req, res) => {
@@ -16,18 +16,33 @@ router.get("/", async (req, res) => {
 
     //   // Serialize data so the template can read it
     //   const projects = projectData.map((project) => project.get({ plain: true }));
-    const posts = [
-      {
-        title: "hello",
-        description: "Greeting",
-        id: 1,
-        // date_created: Date.now(),
-        user: { name: "xavier" },
-      },
-    ];
+    const postsdata = await Post.findAll({
+      // attributes: ["title", "content","date_created"],
+      include: [{ model: User }],
+    });
+    // convert to plain javascript
+    const posts = postsdata.map((post) => post.get({ plain: true }));
+    console.log(posts);
     // Pass serialized data and session flag into template
     res.render("homepage", {
       posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postsdata = await Post.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+    // convert to plain javascript
+    const post = postsdata.get({ plain: true });
+    console.log(post);
+    // Pass serialized data and session flag into template
+    res.render("post", {
+      ...post,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,2 +1,36 @@
 const sequelize = require("../config/connection");
-const {} = require("../models");
+const { Post, User, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const postList = [];
+  for (const post of postData) {
+    const postItem = await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+    postList.push(postItem);
+  }
+
+  for (const comment of commentData) {
+    const commentItem = await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: postList[Math.floor(Math.random() * postList.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
