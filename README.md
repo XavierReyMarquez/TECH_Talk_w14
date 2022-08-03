cf866be95a5967f833a27585660dc3d27d489cd9
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-08-02 19:25:02 -0700

    adding readme and fixing issues with comments

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # TECH_Talk_w14
+
+## Table of contents
+
+- [General info](#general-info)
+- [Screenshots](#screenshots)
+- [Licenses](#technologies)
+- [Setup](#setup)
+- [Status](#status)
+- [Contact](#contact)
+
+## General info
+
+A tech Blog made with mysql to help understand MVC
+
+## Screenshots
+
+![Example Tech Blog](./assets/images/main%20page.PNG)
+
+![Example Tech Post](./assets/images/post.PNG)
+
+![Example Tech Create Post](./assets/images/create%20post.PNG)
+
+## Licenses
+
+- MIT
+
+## Setup
+
+run npm install
+
+## Code
+
+- HTML/CSS
+- JavaScript
+- Node.js
+
+## Status
+
+Project is Finished.
+
+## Contact
+
+Created by Xavier Marquez
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,13 +1,14 @@
 const newcommentHandler = async (event) => {
   const content = document.querySelector("#user-comment").value.trim();
+  const post_id = location.href.split("/")[location.href.split("/").length - 1];
   if (content) {
-    const response = fetch("/api/comments", {
+    const response = await fetch("/api/comments", {
       method: "POST",
-      body: JSON.stringify({ content }),
+      body: JSON.stringify({ content, post_id }),
       headers: { "Content-Type": "application/json" },
     });
     const result = await response.json();
-
+    console.log(result);
     if (response.ok) {
       document.location.reload();
     } else {
@@ -15,7 +16,7 @@ const newcommentHandler = async (event) => {
     }
   }
 };
-console.log(result);
+
 document
   .querySelector("#comment-button")
   .addEventListener("click", newcommentHandler);
